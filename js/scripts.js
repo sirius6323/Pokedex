c92da250c7d1d2aac2dce55548a56e3831e86146
sirius6323 <sirius6323@gmail.com>
2021-02-14 13:36:15 -0800

    If applied, will have weight spelled correctly

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ pokemonList[0] = {
 	name: 'Bulbasaur',
 	type: ['Grass', 'Poison'],
 	height: 0.7,
-	wight: 6.9
+	weight: 6.9
 },
 
 pokemonList[1] = {
 	name: 'Charmander',
 	type: 'Fire',
 	height: 0.6,
-	wight: 8.5
+	weight: 8.5
 },
 
 pokemonList[2] = {
 	name: 'Squirtle',
 	type: 'Water',
 	height: 0.5,
-	wight: 9
+	weight: 9
 };
 
