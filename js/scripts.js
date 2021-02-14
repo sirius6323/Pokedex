be61aa88eb8c33dc151c1aec1e63b1cbeaf26fc0
sirius6323 <sirius6323@gmail.com>
2021-02-14 12:00:03 -0800

    If applied, this commit created pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 // Creates an empty Array for Pokemon 
 let pokemonList = [];
 
+// Created pokemon Objects 
+let pokemon1 = {
+	name: 'Bulbasaur',
+	type: ['Grass', 'Poison'],
+	height: 0.7,
+	wight: 6.9
+};
+
+let pokemon2 = {
+	name: 'Charmander',
+	type: 'Fire',
+	height: 0.6,
+	wight: 8.5
+};
+
+let pokemon3 = {
+	name: 'Squirtle',
+	type: 'Water',
+	height: 0.5,
+	wight: 9
+};
+
