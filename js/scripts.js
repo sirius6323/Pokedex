81909c6b9fbc8d6559d8db57a7ff8333142c91b1
sirius6323 <sirius6323@gmail.com>
2021-02-14 13:32:26 -0800

    If applied, this will have added pokemon to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 // Creates an empty Array for Pokemon 
 const pokemonList = [];
 
-// Created pokemon Objects 
-let pokemon1 = {
+// Adds pokemon to pokemonList Array  
+pokemonList[0] = {
 	name: 'Bulbasaur',
 	type: ['Grass', 'Poison'],
 	height: 0.7,
 	wight: 6.9
-};
+},
 
-let pokemon2 = {
+pokemonList[1] = {
 	name: 'Charmander',
 	type: 'Fire',
 	height: 0.6,
 	wight: 8.5
-};
+},
 
-let pokemon3 = {
+pokemonList[2] = {
 	name: 'Squirtle',
 	type: 'Water',
 	height: 0.5,
