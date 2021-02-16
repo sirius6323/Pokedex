c0323ae4ae5b20bd72d900c267ea6da20878e9cd
sirius6323 <sirius6323@gmail.com>
2021-02-15 23:25:42 -0800

    If applied, comment was updated to display height

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ pokemonList[2] = {
 	weight: 9
 };
 
-// Array that prints pokemon with their weight 
+// Array that prints pokemon with their height 
 for (let i = 0; i < pokemonList.length; i++) {
   document.write(`${pokemonList[i].name} (height: ${pokemonList[i].weight} kg)<br>`);
 }
