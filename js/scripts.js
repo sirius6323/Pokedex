707b3562ac092e73f1474bace98a1d274c3b87a3
sirius6323 <sirius6323@gmail.com>
2021-03-16 18:22:04 -0700

    if applied, added openModal to showDetails function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepo = (function () {
 	let pokemonList = [];
 	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-	let modal = document.querySelector('#modal');
+	const modalContainer = document.querySelector('#overlay');
 
 	// Capitalizes the name of each pokemon
 	function capitalize(name) {
@@ -51,7 +51,93 @@ let pokemonRepo = (function () {
 	// Prints to console on pokemon that was clicked
 	function showDetails(pokemon) {
 		loadDetails(pokemon).then(() => {
-			console.log(pokemon);
+
+			// Function to display modal of pokemon
+			function openModal(pokemon) {
+				
+				// Cleasrs all exisiting modal content
+				modalContainer.innerHTML = '';
+
+				// Creates modal 
+				const pokeModal = document.createElement('div');
+				pokeModal.classList.add('modal');
+				pokeModal.setAttribute('id', 'modal');
+
+				// Creates modal header
+				const modalHeader = document.createElement('div');
+				modalHeader.classList.add('modal-header');
+
+				// Creates modal title 
+				const pokeTitle = document.createElement('div');
+				pokeTitle.classList.add('title');
+				pokeTitle.innerText = title;
+
+				// Creates modal close button 
+				const closeButton = document.createElement('button');
+				closeButton.classList.add('close-button');
+				closeButton.innerHTML = '&times;';
+				closeButton.addEventListener('click', closeModal);
+
+				// Creates modal body
+				const modalBody = document.createElement('div');
+				modalBody.classList.add('modal-body');
+
+				// Creates stats for pokemon
+				const pokeType = document.createElement('h4');
+				pokeType.innerText = text;
+
+				const pokeHeight = document.createElement('h4');
+				pokeHeight.innerText = 'Pokemon Height';
+
+				const pokeWeight = document.createElement('h4');
+				pokeWeight.innerText = 'Pokemon Weight';
+
+				const pokeImg = document.createElement('img');
+				pokeImg.src = 'pokemon.imageUrl';
+				pokeImg.setAttribute('alt', 'A high resolution sprite image of pokemon.');
+
+				// Appends created modal and contents to modal container 
+				modalContainer.appendChild(pokeModal);
+				pokeModal.appendChild(modalHeader);
+				modalHeader.appendChild(pokeTitle);
+				modalHeader.appendChild(closeButton);
+				pokeModal.appendChild(modalBody);
+				modalBody.appendChild(pokeType);
+				modalBody.appendChild(pokeHeight);
+				modalBody.appendChild(pokeWeight);
+				modalBody.appendChild(pokeImg);
+
+				// Adds active class to modalContainer and pokeModal to display 
+				modalContainer.classList.add('active');
+				pokeModal.classList.add('active');
+
+			}
+
+			// Function to close modal 
+			function closeModal() {
+				modalContainer.classList.remove('acitve');
+
+				// Closes modal with the Esc key 
+				window.addEventListener('keydown', (e) => {
+					if (e.key === 'Escape' && modalContainer.classList.contains('active')) {
+						closeModal();
+					}
+				})
+			}
+
+			// Closes modal when clicked outside on overlay 
+			modalContainer.addEventListener('click', (e) => {
+				let target = e.target;
+				if (target === modalContainer) {
+					closeModal();
+				}
+			});
+
+			// Displays modal and overlay
+			document.querySelector('[data-modal-target]').addEventListener('click', () => {
+				openModal(pokemon);
+			});
+
 		});
 	}
 
@@ -119,7 +205,7 @@ let pokemonRepo = (function () {
 		addListItem: addListItem,
 		loadList: loadList,
 		loadDetails: loadDetails,
-		showDetails: showDetails,
+		showDetails: showDetails
 	};
 })();
 
